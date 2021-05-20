bcd29261fc9a429d11cdb378a3d00bc7c4032ee0
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-05-20 15:30:12 +0200

    workstation ready

---
+++ milestone1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Dischi milestone 1</title>
+
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,500;0,600;1,400&display=swap" rel="stylesheet">
+</head>
+<body>
+
+
+    <!-- php -   Solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi -->
+
+    <header>
+
+
+    </header>
+
+    <main>
+        <section>
+        
+        </section>
+    </main>
+    
+</body>
+</html>
