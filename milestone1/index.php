7d9fd9fba385b5d387f55a6ae448904d886447ff
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-05-20 15:45:40 +0200

    style

--- milestone1/index.php
+++ milestone1/index.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php Dischi milestone 1</title>
 
+    <!-- css -->
+    <link rel="stylesheet" href="../css/main.css">
+
     <!-- font -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,500;0,600;1,400&display=swap" rel="stylesheet">
--- milestone1/templates/header.php
+++ milestone1/templates/header.php
@@ -1,7 +1,13 @@
 <header>
 
-    <div class="box-img">
-        <img src="../img/logo.png" alt="logo spotify">
+
+    <div class="container">
+
+        <div class="box-img">
+            <img src="../img/logo.png" alt="logo spotify">
+        </div>
     </div>
+
+    
 
 </header>
