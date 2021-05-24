9abb97c0669c18a6b721a5df95a8d8b9365eb34b
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-05-24 15:59:33 +0200

    add v-for

--- milestone2/js/tool.js
+++ milestone2/js/tool.js
@@ -3,35 +3,47 @@
 const app = new Vue({
     el: '#app',
     data: {
-        album: [],
-        artisti:[],
-        value: 'all',
+        albums: [],
+        artists: [],
+        filterArtist: 'all',
+        
 
     },
     created() {
         console.log(window.location);
         //http://localhost/php-ajax-dischi/milestone2/
         const apiURL = window.location.href + 'database.php'; 
-        
-        
-        this.getData(apiURL);
-            
 
-    },
-    methods:{
-        getData(url){
-            axios.get(url)
-            .then(res =>{
+        axios
+            .get(apiURL)
+            .then(res => {
                 console.log(res.data);
-                this.album = res.data;
+                this.albums = res.data.albums;
+                this.artists = res.data.artists;
             })
             .catch(err =>{
-                console.log(err, 'Error');
-            })
-        },
-        return(){
-            console.log('ritorno');
-        }
+                console.log(err);
+            });
+        
+    },    
+    //     this.getData(apiURL);
+            
 
-    }
+    // },
+    // methods:{
+    //     getData(url){
+    //         axios.get(url)
+    //         .then(res =>{
+    //             console.log(res.data);
+    //             this.album = res.data;
+    //         })
+    //         .catch(err =>{
+    //             console.log(err, 'Error');
+    //         })
+    //     },
+    //     return(){
+    //         console.log('ritorno');
+    //     }
+
+    // }
 });
